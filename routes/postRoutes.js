22173c12609b63990a389bb5cd4a670d56e43d01
tfermoso <tomas.fermoso@gmail.com>
2025-05-05 17:01:00 +0200

    corregimos orden rutas

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 const verifyToken = require('../middlewares/verifyToken');
 
+router.get('/my', verifyToken, postController.getPostsByUser);
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPostById);
 router.post('/', verifyToken, postController.createPost);
-router.get('/my', verifyToken, postController.getPostsByUser);
 
 module.exports = router;
